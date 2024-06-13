4a2edf53ec2d024255b41a5091597e36acc4ba2c
dangduongcoder <truongdangduong666@gmail.com>
2024-06-13 11:08:16 +0700

    update: hack 8bp iosviet

---
+++ 8bp-hack-iosviet.js
@@ -0,0 +1,19 @@
+/*******************************
+
+ CRACKED BY DANG DUONG
+
+ *******************************
+
+ [rewrite_local]
+
+ ^http[s]?:\/\/iosviet.co\/check-lic url script-response-body 8bp-hack-iosviet.js
+
+ [mitm]
+
+ hostname = iosviet.co
+
+ *******************************/
+
+var body = '0BTqlsUsIAbCyckkDFMWP9E18xJNKWSLpm88ZCWjm1Q7ODV9cX2vBI4RvntzIQ3turg72x8auAwxyYtIg1z4r2bOOvcpnOUOuCZpWPiRpBUb4oaRkVBph3tGhPkNs+fJ648CU9s1DBKxirlXYLeGp9/x6BaZ7Pp6sDcpr1RzVozMH1BLfnc2tZGrHxmk9O11Fw87rdTulik7Qep62d+ehePvcUMSSVLLVsjDcxJAJj4lx6S9/v1WfPvvSK5LZQA6ld7JF2N9HgxCSi45sJHqrk4vxqeGLfYFPAlB0uEmB9ua49yaJQqtfO6Q87sptnq6xdOLAXQajZ79i3nIdkSX7emTAADl4i11lYzSFuhxWAI0e2jZTPTUjDhBfnq/rqtVC77pouMg+yLbsUP/jJKvRv0JIfFt9ljBG5MUIPFNwTAkuLmQjamjgkUz9rPNFcZckD3k3nZqKv1/3TaAOaQHjSXi0ipcWb1FO8f8nzZ2Rej53ro2ugJrq+eIraBLKfr3dWCE8vseqtCZPCoBVvH8oGzW9pVgMtTJYOKyeN5LQ3fXkCZWtfrnMtdeKO9jA2Jg';
+
+$done({body : body});
